389f3fa62b931bb3b72469b760454f5e08df663d
sami.ekblad@itmill.com <sami.ekblad@itmill.com@de684a11-831f-0410-ab27-cb86445eb682>
2010-05-23 12:38:25 +0000

    Upgraded vaadin library. Added click support.
    git-svn-id: http://dev.vaadin.com/svn/contrib/Overlays@13308 de684a11-831f-0410-ab27-cb86445eb682

--- src/org/vaadin/overlay/OverlaySampleApplication.java
+++ src/org/vaadin/overlay/OverlaySampleApplication.java
@@ -19,73 +19,77 @@ public class OverlaySampleApplication extends Application {
         setMainWindow(mainWindow);
 
 
-        Button button = new Button("Sample Button");
-        mainWindow.addComponent(button);
-
-        final Label label = new Label("");
-        label.setValue("Alignment.TOP_LEFT");
+        final Label label = new Label("Alignment.TOP_LEFT");
         mainWindow.addComponent(label);
 
-        final ImageOverlay io = new ImageOverlay(button);
-        Resource res = new ClassResource(this.getClass(), "icon-new.png", this);
-        io.setImage(res);
-        io.setComponentAnchor(Alignment.TOP_LEFT); // Top left of the button
-        io.setOverlayAnchor(Alignment.MIDDLE_CENTER); // Center of the image
-        io.setClickListener(new OverlayClickListener() {
+        for (int i = 0; i < 20; i++) {
 
-            public void overlayClicked(CustomClickableOverlay overlay) {
-                mainWindow.showNotification("ImageOverlay Clicked!");
-            }
-        });
-        mainWindow.addComponent(io);
+            Button button = new Button("Sample Button");
+            mainWindow.addComponent(button);
 
-        final TextOverlay to = new TextOverlay(button, "New!");
-        to.setComponentAnchor(Alignment.TOP_LEFT); // Top left of the button
-        to.setOverlayAnchor(Alignment.MIDDLE_CENTER); // Center of the image
-        to.setClickListener(new OverlayClickListener() {
+            final ImageOverlay io = new ImageOverlay(button);
+            Resource res = new ClassResource(this.getClass(), "icon-new.png",
+                    this);
+            io.setImage(res);
+            io.setComponentAnchor(Alignment.TOP_LEFT); // Top left of the button
+            io.setOverlayAnchor(Alignment.MIDDLE_CENTER); // Center of the image
+            io.setClickListener(new OverlayClickListener() {
 
-            public void overlayClicked(CustomClickableOverlay overlay) {
-                mainWindow.showNotification("TextOverlay Clicked!");
-            }
-        });
-        mainWindow.addComponent(to);
-
-        button.addListener(new Button.ClickListener() {
-
-            private static final long serialVersionUID = 1L;
-
-            public void buttonClick(ClickEvent event) {
-                Alignment a = io.getComponentAnchor();
-                String s = "";
-                if (Alignment.TOP_LEFT == a) {
-                    a = Alignment.TOP_CENTER;
-                    s = "TOP_CENTER";
-                } else if (Alignment.TOP_CENTER == a) {
-                    a = Alignment.TOP_RIGHT;
-                    s = "TOP_RIGHT";
-                } else if (Alignment.TOP_RIGHT == a) {
-                    a = Alignment.MIDDLE_RIGHT;
-                    s = "MIDDLE_RIGHT";
-                } else if (Alignment.MIDDLE_RIGHT == a) {
-                    a = Alignment.BOTTOM_RIGHT;
-                    s = "BOTTOM_RIGHT";
-                } else if (Alignment.BOTTOM_RIGHT == a) {
-                    a = Alignment.BOTTOM_CENTER;
-                    s = "BOTTOM_CENTER";
-                } else if (Alignment.BOTTOM_CENTER == a) {
-                    a = Alignment.BOTTOM_LEFT;
-                    s = "BOTTOM_LEFT";
-                } else if (Alignment.BOTTOM_LEFT == a) {
-                    a = Alignment.MIDDLE_LEFT;
-                    s = "MIDDLE_LEFT";
-                } else if (Alignment.MIDDLE_LEFT == a) {
-                    a = Alignment.TOP_LEFT;
-                    s = "TOP_LEFT";
+                public void overlayClicked(CustomClickableOverlay overlay) {
+                    mainWindow.showNotification("ImageOverlay Clicked!");
                 }
-                io.setComponentAnchor(a);
-                label.setValue("Alignment." + s);
-            }
-        });
+            });
+            mainWindow.addComponent(io);
+
+            final TextOverlay to = new TextOverlay(button, "New!");
+            to.setComponentAnchor(Alignment.TOP_LEFT); // Top left of the button
+            to.setOverlayAnchor(Alignment.MIDDLE_CENTER); // Center of the image
+            to.setClickListener(new OverlayClickListener() {
+
+                public void overlayClicked(CustomClickableOverlay overlay) {
+                    mainWindow.showNotification("TextOverlay Clicked!");
+                }
+            });
+            mainWindow.addComponent(to);
+
+            button.addListener(new Button.ClickListener() {
+
+                private static final long serialVersionUID = 1L;
+
+                public void buttonClick(ClickEvent event) {
+                    Alignment a = io.getComponentAnchor();
+                    String s = "";
+                    if (Alignment.TOP_LEFT == a) {
+                        a = Alignment.TOP_CENTER;
+                        s = "TOP_CENTER";
+                    } else if (Alignment.TOP_CENTER == a) {
+                        a = Alignment.TOP_RIGHT;
+                        s = "TOP_RIGHT";
+                    } else if (Alignment.TOP_RIGHT == a) {
+                        a = Alignment.MIDDLE_RIGHT;
+                        s = "MIDDLE_RIGHT";
+                    } else if (Alignment.MIDDLE_RIGHT == a) {
+                        a = Alignment.BOTTOM_RIGHT;
+                        s = "BOTTOM_RIGHT";
+                    } else if (Alignment.BOTTOM_RIGHT == a) {
+                        a = Alignment.BOTTOM_CENTER;
+                        s = "BOTTOM_CENTER";
+                    } else if (Alignment.BOTTOM_CENTER == a) {
+                        a = Alignment.BOTTOM_LEFT;
+                        s = "BOTTOM_LEFT";
+                    } else if (Alignment.BOTTOM_LEFT == a) {
+                        a = Alignment.MIDDLE_LEFT;
+                        s = "MIDDLE_LEFT";
+                    } else if (Alignment.MIDDLE_LEFT == a) {
+                        a = Alignment.TOP_LEFT;
+                        s = "TOP_LEFT";
+                    }
+                    io.setComponentAnchor(a);
+                    label.setValue("Alignment." + s);
+                }
+            });
+
+        }
 
     }
 }
